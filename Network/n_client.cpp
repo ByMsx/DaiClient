c847d449300402fc3b9209323f767dd3bdd30243
Кирилл Зимников <kirill@zimnikov.ru>
2018-09-10 10:25:59 +0700

    Убрал отладочные сообщения

--- Network/n_client.cpp
+++ Network/n_client.cpp
@@ -374,10 +374,6 @@ void Client::proccessMessage(quint16 cmd, QDataStream &msg)
             Helpz::parse_out(msg, log_type, date_ms);
             if (log_type != Dai::ValueLog && log_type != Dai::EventLog)
                 break;
-
-            qCDebug(NetClientLog) << "Request sync" << (log_type == Dai::ValueLog ? "values" : "events")
-                                  << "range from" << date_ms;
-
             send(cmd) << log_type << getLogRange(log_type, date_ms);
         }
         break;
@@ -390,10 +386,6 @@ void Client::proccessMessage(quint16 cmd, QDataStream &msg)
             break;
 
         auto res = getLogData(log_type, range);
-
-        qCDebug(NetClientLog) << "Request sync" << (log_type == Dai::ValueLog ? "values" : "events")
-                              << "from:" << range.first << "to:" << range.second;
-
         try {
             switch (log_type) {
             case ValueLog: send(cmd) << log_type << res.not_found << std::get<0>(res.data); qDebug() << std::get<0>(res.data).size(); break;
